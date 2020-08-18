b52ff5dd6a7463d54225bfa75a4a464a29e1fa21
KaiserCristoph <69873677+KaiserCristoph@users.noreply.github.com>
2020-08-18 18:04:05 -0300

    "Inicio"

---
+++ src/DatabaseManager.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Locale;
+
+public class DatabaseManager {
+	
+	private static Connection databaseConnection;
+	
+	private static String Connection_String = "jdbc:oracle:thin:@localhost:1521:xe";
+	private static String usuario = "system";
+	private static String contraseña = "12345";
+	
+	
+	static{
+		try {
+			Locale.setDefault(new Locale("es","ES"));
+			databaseConnection = DriverManager.getConnection(Connection_String, usuario, contraseña);
+			
+		} catch (SQLException e) {
+			System.out.println("Error al conectarse a la Base de datos");
+			System.out.println(e.getErrorCode());
+		}
+	}
+	
+	public static Connection getConnection(){
+		return databaseConnection;
+	}
+
+}
