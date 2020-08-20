4b71974a17ce0e3a4edfc51299a36ce4203b2fa0
Sergio1304 <69941011+Sergio1304@users.noreply.github.com>
2020-08-19 22:09:40 -0300

    tuya jorge

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import java.util.Locale;
 
 public class DatabaseManager {
-	//lo que salga
+	
 	private static Connection databaseConnection;
 	
 	private static String Connection_String = "jdbc:oracle:thin:@localhost:1521:xe";
@@ -13,15 +13,35 @@ public class DatabaseManager {
 	
 	
 	static{
-		try {
-			Locale.setDefault(new Locale("es","ES"));
-			databaseConnection = DriverManager.getConnection(Connection_String, usuario, contraseña);
+			//se agrego 
+			databaseConnection = null;
 			
-		} catch (SQLException e) {
-			System.out.println("Error al conectarse a la Base de datos");
-			System.out.println(e.getErrorCode());
-		}
+		try {
+			
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				
+				System.out.println("Esta el driver para conectar Oracle, La libreira esta referenciada");
+				
+				try {//se agrega instanciar el objeto conectio
+					
+					//Locale.setDefault(new Locale("es","ES"));
+					databaseConnection = DriverManager.getConnection(Connection_String, usuario, contraseña);
+					//se agrega impresion de mensaje conexion realizada
+					System.out.println("Conexion creada con exito");
+			
+				} catch (SQLException e) {
+					System.out.println("Error al conectarse a la Base de datos");
+					//se elimino el System.out.println, no corresponde
+					e.printStackTrace();
+				}
+				//se agrega excepcion donde no el msj imprime que no se tiene los driver para realizar la conexion
+	}catch (ClassNotFoundException e) {
+		
+		System.out.println("Tu build-path no cuneta con el driver?");
+		e.printStackTrace();
 	}
+		
+	}//se agrega corchete
 	
 	public static Connection getConnection(){
 		return databaseConnection;
