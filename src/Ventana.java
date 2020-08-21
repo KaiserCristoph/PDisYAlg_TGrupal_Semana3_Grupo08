cc64dc8d5f1b1669b0cfb34095e61ba1ce2d4054
GabiGolx <gabriel.ramirez@estudiantes.utec.edu.uy>
2020-08-21 15:35:33 -0300

    Update Ventana.java
    Actualizacion Ventana

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,12 +1,22 @@
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class Ventana {
 
@@ -18,6 +28,9 @@ public class Ventana {
 	private JButton btnBuscar;
 	private JButton btnModificar;
 	private JButton btnMostrarTodo;
+	private JTextField txtCedula;
+	private JTextField txtApellido;
+	private JTextField txtNombre;
 
 	/**
 	 * Launch the application.
@@ -49,8 +62,123 @@ public class Ventana {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		JPanel contentPane = new JPanel();
+		contentPane.setBackground(new Color(176, 196, 222));
+		contentPane.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		frame.setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(50, 68, 189, 33);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		txtCedula = new JTextField();
+		txtCedula.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if(txtCedula.getText().equals("Cedula")) {
+					txtCedula.setText("");
+				}
+				else {
+					txtCedula.selectAll();
+				}
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(txtCedula.getText().equals("")) {
+					txtCedula.setText("Cedula");
+				}
+			}
+		});
+		txtCedula.setText("Cedula");
+		txtCedula.setBounds(0, 0, 189, 33);
+		panel.add(txtCedula);
+		txtCedula.setColumns(10);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(255, 255, 255));
+		panel_1.setBounds(50, 123, 189, 33);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		txtApellido = new JTextField();
+		txtApellido.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if(txtApellido.getText().equals("Apellido")) {
+					txtApellido.setText("");
+				}
+				else {
+					txtApellido.selectAll();
+				}
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(txtApellido.getText().equals("")) {
+					txtApellido.setText("Apellido");
+				}
+			}
+		});
+		txtApellido.setText("Apellido");
+		txtApellido.setColumns(10);
+		txtApellido.setBounds(0, 0, 189, 33);
+		panel_1.add(txtApellido);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(null);
+		panel_2.setBackground(new Color(255, 255, 255));
+		panel_2.setBounds(50, 177, 189, 33);
+		contentPane.add(panel_2);
+		panel_2.setLayout(null);
+		
+		txtNombre = new JTextField();
+		txtNombre.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if(txtNombre.getText().equals("Nombre")) {
+					txtNombre.setText("");
+				}
+				else {
+					txtNombre.selectAll();
+				}
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(txtNombre.getText().equals("")) {
+					txtNombre.setText("Nombre");
+				}
+			}
+		});
+		txtNombre.setText("Nombre");
+		txtNombre.setColumns(10);
+		txtNombre.setBounds(0, 0, 189, 33);
+		panel_2.add(txtNombre);
+		
+		JLabel lblNewLabel = new JLabel("X");
+		lblNewLabel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(JOptionPane.showConfirmDialog(null, "Estas seguro que deseas salir?", "Confirmar", JOptionPane.YES_NO_OPTION) == 0);{			
+						System.exit(0);	
+				}
+			}
+			public void mouseEntered(MouseEvent e) {
+				lblNewLabel.setForeground(Color.WHITE);
+			}
+			public void mouseExited(MouseEvent e) {
+				lblNewLabel.setForeground(Color.BLACK);
+			}
+		});
+		lblNewLabel.setForeground(new Color(0, 0, 0));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(431, 0, 19, 20);
+		contentPane.add(lblNewLabel);
+		frame.setUndecorated(true);
+		frame.setLocationRelativeTo(null);
+		
+		/*
 		textCedula = new JTextField();
 		textCedula.setBounds(132, 36, 86, 20);
 		frame.getContentPane().add(textCedula);
@@ -65,71 +193,73 @@ public class Ventana {
 		textNombre.setBounds(132, 69, 86, 20);
 		frame.getContentPane().add(textNombre);
 		textNombre.setColumns(10);
-		
+		*/
 		JButton btnAlta = new JButton("Alta");
 		btnAlta.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				String ci=textCedula.getText();
-				String nombre=textNombre.getText();
-				String apellido=textApellido.getText();
+				String ci=txtCedula.getText();
+				String nombre=txtNombre.getText();
+				String apellido=txtApellido.getText();
 				Empleado e = new Empleado(ci,nombre,apellido);
 				DAOEmpleados.insert(e);
 				JOptionPane.showMessageDialog(null, "Exito","Exito", JOptionPane.INFORMATION_MESSAGE);
 			}
 		});
-		btnAlta.setBounds(254, 35, 89, 23);
+		btnAlta.setBounds(285, 68, 116, 23);
 		frame.getContentPane().add(btnAlta);
 		
 		btnEliminar = new JButton("Eliminar");
 		btnEliminar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String ci=textCedula.getText();
-				String nombre=textNombre.getText();
-				String apellido=textApellido.getText();
+				String ci=txtCedula.getText();
+				String nombre=txtNombre.getText();
+				String apellido=txtApellido.getText();
 				Empleado e1 = new Empleado(ci,nombre,apellido);
 				DAOEmpleados.delete(e1);
 			}
 		});
-		btnEliminar.setBounds(254, 83, 89, 23);
+		btnEliminar.setBounds(285, 107, 116, 23);
 		frame.getContentPane().add(btnEliminar);
 		
 		btnBuscar = new JButton("Buscar");
 		btnBuscar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				String ci=textCedula.getText();
+				String ci=txtCedula.getText();
 			
 				Empleado re=DAOEmpleados.find(ci);
-				textNombre.setText(re.getNombre());
-				textApellido.setText(re.getApellido());
+				txtNombre.setText(re.getNombre());
+				txtApellido.setText(re.getApellido());
 			}
 		});
-		btnBuscar.setBounds(254, 128, 89, 23);
+		btnBuscar.setBounds(285, 146, 116, 23);
 		frame.getContentPane().add(btnBuscar);
 		
 		btnModificar = new JButton("Modificar");
 		btnModificar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String ci=textCedula.getText();
-				String nombre=textNombre.getText();
+				String ci=txtCedula.getText();
+				String nombre=txtNombre.getText();
 				String apellido=textApellido.getText();
 				Empleado e1 = new Empleado(ci,nombre,apellido);
 				DAOEmpleados.update(e1);
 			}			
 		});
-		btnModificar.setBounds(254, 176, 89, 23);
+		btnModificar.setBounds(285, 185, 116, 23);
 		frame.getContentPane().add(btnModificar);
 		
 		btnMostrarTodo = new JButton("Mostrar Todo");
+		btnMostrarTodo.setBackground(new Color(255, 255, 255));
+		btnMostrarTodo.setForeground(new Color(0, 0, 0));
 		btnMostrarTodo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) { 
 				MostrarTodo mt=new MostrarTodo();
 				mt.mostrar();
 			}
 		});
-		btnMostrarTodo.setBounds(61, 176, 157, 23);
+		btnMostrarTodo.setBounds(137, 251, 189, 33);
 		frame.getContentPane().add(btnMostrarTodo);
-		
+		/*
 		JLabel lblCedula = new JLabel("cedula");
 		lblCedula.setBounds(48, 39, 46, 14);
 		frame.getContentPane().add(lblCedula);
@@ -141,5 +271,6 @@ public class Ventana {
 		JLabel lblApellido = new JLabel("Apellido");
 		lblApellido.setBounds(48, 117, 46, 14);
 		frame.getContentPane().add(lblApellido);
+		*/
 	}
 }
